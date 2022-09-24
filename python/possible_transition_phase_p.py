40ffe6f0b8de2ceb8d0bc6a34e5dcc6023e95195
moto <mo_to1015@yahoo.co.jp>
2022-09-24 14:36:31 +0900

    グラフ追加

---
+++ python/possible_transition_phase_p.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+
+# https://oku.edu.mie-u.ac.jp/~okumura/python/hist-median.html ˆø—p
+def hquantile(hist, q=0.5):
+    n = sum(hist)
+    t = n * q
+    i = 0
+    s = hist[i]
+    while s < t:
+        i += 1
+        s += hist[i]
+    if s == t and hist[i + 1] == 0:
+        j = i + 2
+        while hist[j] == 0:
+            j += 1
+        return (i + j) / 2
+    return i + (t - s) / hist[i] + 0.5
+
+
+def f(p, hist):
+    if p - math.floor(p) < 0.5:
+        return hist[math.floor(p)]
+    elif p - math.floor(p) > 0.5:
+        return hist[math.ceil(p)]
+    else:
+        return (hist[math.floor(p)] + hist[math.ceil(p)]) / 2
+
+
+if __name__ == "__main__":
+    parent_parent = Path(__file__).resolve().parent.parent
+    df = pd.read_csv(
+        parent_parent.joinpath(Path("output/possible_transition_phase_p_4.csv")),
+        header=0,
+    )
+
+    df = df[["proportion", "count"]].values
+    df = df[np.argsort(df[:, 0])]
+    df = np.transpose(df)
+    df = df.tolist()
+    df[1] = list(map(int, df[1]))
+    mn = min(df[0])
+    mx = max(df[0])
+    cnt = sum(df[1])
+    mean = sum([df[0][i] * df[1][i] / cnt for i in range(len(df[0]))])
+    std = math.sqrt(
+        sum([(df[0][i] - mean) ** 2 * df[1][i] / cnt for i in range(len(df[0]))])
+    )
+
+    p1 = hquantile(df[1], 0.25) - 1
+    p2 = hquantile(df[1], 0.5) - 1
+    p3 = hquantile(df[1], 0.75) - 1
+
+    print("count", str(cnt).rjust(32))
+    print("mean", str(mean).rjust(33))
+    print("std", str(std).rjust(34))
+    print("min", str(mn).rjust(34))
+    print("25%", str(f(p1,df[0])).rjust(34))
+    print("50%", str(f(p2,df[0])).rjust(34))
+    print("75%", str(f(p3,df[0])).rjust(34))
+    print("max", str(mx).rjust(34))
+
+    plt.hist(df[0], bins=50, weights=df[1])
+    plt.title("possible_transition_phase_p_4")
+    plt.xlabel("proportion")
+    plt.ylabel("quantity")
+    plt.yscale("log")
+    plt.show()
